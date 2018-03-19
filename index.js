fdad079e01e053e8c0a6e0da280271029c9decb7
Michael Roth <mick2090@gmail.com>
2018-03-19 23:49:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(katzDeliLine){
   if(katzDeliLine.length === 0){
     return 'The line is currently empty.';
   }else{
-  var arr = [];
+   var arr = [];
    for(var i=0; i<katzDeliLine.length; i++){
       arr.push(` ${i+1}. ${katzDeliLine[i]}`);
    }
